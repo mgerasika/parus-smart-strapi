0b126b63f78afe2a937844bf653b6ba553e6137a
Mykhaylo Gerasika <mger@ciklum.com>
2022-01-01 08:07:08 +0200

    1

--- src/api/attachment/routes/attachment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * attachment router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::attachment.attachment');
--- src/api/attachment/services/attachment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * attachment service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::attachment.attachment');
